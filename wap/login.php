63205c109c575852b24a03a9dd4acd81e8a47890
jiaown <316231881@qq.com>
2015-09-28 17:26:56 +0800

    登录页面ing

---
+++ wap/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<title>用户登录</title>
+<meta charset="gb2312">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/global.css" rel="stylesheet">
+<link href="css/default.css" rel="stylesheet">
+</head>
+<body>
+<div class="ui_header">
+    <div class="wrapper">
+        <a href="" class="pre"></a>
+        <span>登录系统</span>
+        <a href="" class="search"></a>
+        <a href="" class="person"></a>
+    </div>
+</div>
+<div class="wrapper loginform">
+
+</div>
+<div class="ui_footer">
+	<div class="wrapper">
+    	<div class="telephone">
+        	<a href=""><img src="images/telephone.png"></a>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
