44d0a87939ce86ab333a25d3de3f1433d3f965e3
jiaown <316231881@qq.com>
2015-09-29 17:00:00 +0800

    登录页面完成

--- wap/login.php
+++ wap/login.php
@@ -9,25 +9,47 @@
 <link href="css/default.css" rel="stylesheet">
 </head>
 <body>
-<div class="ui_header">
-    <div class="wrapper">
+<div class="ui_header" style="position:relative;">
+	<img class="bg" src="images/f_heder.jpg">
+    <div class="wrapper" style="left:0; top:0; position:absolute;">
         <a href="" class="pre"></a>
+        <a href="#" class="refresh"></a>
         <span>登录系统</span>
         <a href="" class="search"></a>
         <a href="" class="person"></a>
     </div>
+
 </div>
 <div class="wrapper loginform">
+<form action="" method="post">
 	<span>用户名：</span>
     <div class="typein">
     	<img src="images/ico_01.png"/>
     	<input type="text" name="" value="" />
     </div>
     <span>密码：</span>
-    <div class="typein">
+    <div class="typein" style="margin-bottom:30px;">
     	<img src="images/ico_02.png"/>
     	<input type="text" name="" value="" />
     </div>
+    <div style="margin:0 auto; width:50%;">
+    	<input type="submit" value="登录"/>
+    </div>
+</form>
+</div>
+<div class="a_reg">
+	<img class="bg" src="images/login_01.jpg">
+    <a href=""><img class="bg" src="images/login_02.jpg"></a>
+</div>
+<div class="a_link">
+	<div class="sepeteor"></div>
+    <div class="link_main">
+    	<a href="">关于我们</a>
+        <a href=""  class="selected">平台优势</a>
+        <a href="">媒体报道</a>
+        <a href="">联系我们</a>
+    </div>
+    <div class="sepeteor"></div>
 </div>
 <div class="ui_footer">
 	<div class="wrapper">
