af13a0468540b3b36a83a88693c2266b32aa37c8
jiaown <316231881@qq.com>
2015-09-29 09:53:57 +0800

    个人中心头部底部表单完成

--- wap/login.php
+++ wap/login.php
@@ -4,7 +4,7 @@
 <title>用户登录</title>
 <meta charset="gb2312">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />
 <link href="css/global.css" rel="stylesheet">
 <link href="css/default.css" rel="stylesheet">
 </head>
@@ -18,12 +18,41 @@
     </div>
 </div>
 <div class="wrapper loginform">
-
+	<span>用户名：</span>
+    <div class="typein">
+    	<img src="images/ico_01.png"/>
+    	<input type="text" name="" value="" />
+    </div>
+    <span>密码：</span>
+    <div class="typein">
+    	<img src="images/ico_02.png"/>
+    	<input type="text" name="" value="" />
+    </div>
 </div>
 <div class="ui_footer">
 	<div class="wrapper">
     	<div class="telephone">
         	<a href=""><img src="images/telephone.png"></a>
+        </div>
+        <div class="ui_left">
+        	<a href="">
+            	<img src="images/footer_home.png">
+                <span>首页</span>
+            </a>
+            <a href="">
+            	<img src="images/footer_qq.png">
+                <span>QQ咨询</span>
+            </a>
+        </div>
+        <div class="ui_right">
+        	<a href="">
+            	<img src="images/footer_address.png">
+                <span>公司地址</span>
+            </a>
+            <a href="">
+            	<img src="images/footer_shoppingCart.png">
+                <span>购物车</span>
+            </a>
         </div>
     </div>
 </div>
