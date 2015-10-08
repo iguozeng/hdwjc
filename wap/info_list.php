b3f844f31606282e2bcd4b2d8a7533405a16090e
zhizunlanyang <1534010791@qq.com>
2015-10-08 17:17:25 +0800

    20151008

---
+++ wap/info_list.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta charset="gb2312">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<title>华东五金网</title>
+<link href="css/global.css" rel="stylesheet">
+<link href="css/info.css" rel="stylesheet">
+<script src="js/jquery-1.8.3.min.js" type="text/javascript"></script>
+</head>
+<body>
+<div class="info_nav">
+	<div class="yiji_wrap">
+		<div class="yiji_box">
+			<div class="yiji">
+				<h3>新闻资讯</h3><img class="closing" src="images/info_list_01.png"/><img class="opening" src="images/info_list_02.png"/>
+			</div>
+		</div>
+		<ul>
+			<li><a href="">新闻动态</a></li>
+			<li><a href="">行业资讯</a></li>
+			<li><a href="">活动公告</a></li>
+		</ul>
+	</div>
+	<div class="first">
+		<h3>系统架构</h3>
+
+	</div>
+	<div class="first">
+		<h3>五金常识</h3>
+
+	</div>
+	<div class="first">
+		<h3>公信案例</h3>
+
+	</div>
+</div>
+</body>
+</html>
