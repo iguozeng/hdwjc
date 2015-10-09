bea1532f9d1f54567bba22bfd8b1ff96d1a4cfed
zhizunlanyang <1534010791@qq.com>
2015-10-09 14:28:04 +0800

    资讯列表 除下拉菜单

--- wap/info_list.php
+++ wap/info_list.php
@@ -14,7 +14,9 @@
 	<div class="yiji_wrap">
 		<div class="yiji_box">
 			<div class="yiji">
-				<h3>新闻资讯</h3><img class="closing" src="images/info_list_01.png"/><img class="opening" src="images/info_list_02.png"/>
+				<div>
+					<h3>新闻资讯</h3><img class="closing" src="images/info_list_01.png"/><img class="opening" src="images/info_list_02.png"/>
+				</div>
 			</div>
 		</div>
 		<ul>
@@ -23,18 +25,74 @@
 			<li><a href="">活动公告</a></li>
 		</ul>
 	</div>
-	<div class="first">
-		<h3>系统架构</h3>
-
+	<div class="yiji_wrap">
+		<div class="yiji_box">
+			<div class="yiji">
+				<div>
+					<h3>系统架构</h3><img class="closing" src="images/info_list_01.png"/><img class="opening" src="images/info_list_02.png"/>
+				</div>
+			</div>
+		</div>
+		<ul>
+			<li><a href="">新闻动态</a></li>
+			<li><a href="">行业资讯</a></li>
+			<li><a href="">活动公告</a></li>
+		</ul>
 	</div>
-	<div class="first">
-		<h3>五金常识</h3>
-
+	<div class="yiji_wrap">
+		<div class="yiji_box">
+			<div class="yiji">
+				<div>
+					<h3>五金常识</h3><img class="closing" src="images/info_list_01.png"/><img class="opening" src="images/info_list_02.png"/>
+				</div>
+			</div>
+		</div>
+		<ul>
+			<li><a href="">新闻动态</a></li>
+			<li><a href="">行业资讯</a></li>
+			<li><a href="">活动公告</a></li>
+		</ul>
 	</div>
-	<div class="first">
-		<h3>公信案例</h3>
-
+	<div class="yiji_wrap">
+		<div class="yiji_box">
+			<div class="yiji" style="border-right:none;">
+				<div>
+					<h3>公信案例</h3><img class="closing" src="images/info_list_01.png"/><img class="opening" src="images/info_list_02.png"/>
+				</div>
+			</div>
+		</div>
+		<ul>
+			<li><a href="">新闻动态</a></li>
+			<li><a href="">行业资讯</a></li>
+			<li><a href="">活动公告</a></li>
+		</ul>
 	</div>
+	<div class="clear"></div>
+</div>
+<div class="info_list">
+	<ul>
+		<li>
+			<div class="info_pic fl"><a href=""><img src="images/[Temp]20150804154656_32462.jpg" /></a></div>
+			<div class="info_txt fl info_txt1">
+				<a href="">董事长周才炳 参加"一带一路"建设董事长周才炳 参加"一带一路"建设</a>
+				<span class="info_detail">
+					<span class="pubdate">2015-07-21</span><span class="info_intro">7月15日至16日，江苏民营企业参与"一带一路"建设推进会在连云港市召开，来自全省各地的近二百名企业家齐聚连云港市，聚焦"一带一路"交汇点建设，共话交流合作，探寻发7月15日至16日，江苏民营企业参与"一带一路"建设推进会在连云港市召开，来自全省各地的近二百名企业家齐聚连云港市，聚焦"一带一路"交汇点建设，共话交流合作，探寻发</span>
+				</span>
+			</div>
+			<div class="info_right fl"><a href=""><img src="images/info_list_03.png" /></a></div>
+			<div class="clear"></div>
+		</li>
+		<li>
+			<div class="info_txt fl info_txt2">
+				<a href="">董事长周才炳 参加"一带一路"建设董事长周才炳 参加"一带一路"建设</a>
+				<span class="info_detail">
+					<span class="pubdate">2015-07-21</span><span class="info_intro">7月15日至16日，江苏民营企业参与"一带一路"建设推进会在连云港市召开，来自全省各地的近二百名企业家齐聚连云港市，聚焦"一带一路"交汇点建设，共话交流合作，探寻发7月15日至16日，江苏民营企业参与"一带一路"建设推进会在连云港市召开，来自全省各地的近二百名企业家齐聚连云港市，聚焦"一带一路"交汇点建设，共话交流合作，探寻发</span>
+				</span>
+			</div>
+			<div class="info_right fl"><a href=""><img src="images/info_list_03.png" /></a></div>
+			<div class="clear"></div>
+		</li>
+	</ul>
 </div>
 </body>
 </html>
